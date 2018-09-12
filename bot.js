a68f771e461d34571e8c134c50b2d1b0d04fa6c2
mraco <36960271+mraco@users.noreply.github.com>
2018-09-12 12:27:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -185,7 +185,7 @@ function sin_to_hex(i, phase) {
 let place = 0;
 
 client.on('message', message => {
-  if (!message.content.startsWith(PREFIX)) return;
+  if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
   if (message.author.id !== "439187325503930369") return;
